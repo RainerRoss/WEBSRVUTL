337ad44a902b0263af39e20aa5d5c055d5ddf5fb
Rainer Ross <rainer_ross@web.de>
2017-08-14 11:47:27 +0200

    Create HTTPPOSTCLOB_JSON_TABLE.sql

---
+++ Requests/HTTPPOSTCLOB_JSON_TABLE.sql
@@ -0,0 +1,18 @@
+Select x.*
+  from JSON_TABLE(
+     SYSTOOLS.HTTPGETCLOB('http://www.myhofi.com/myapp/websrv01.pgm?id=1',''), 
+     '$'
+     Columns(
+        nested '$.items[*]' columns (
+            "Id"        integer        path 'lax $.id',
+            "Name"      varchar(40)    path 'lax $.name',
+            "Country"   varchar(2)     path 'lax $.country',
+            "City"      varchar(40)    path 'lax $.city',
+            "Street"    varchar(40)    path 'lax $.street', 
+            "Sales"     dec(15, 2)     path 'lax $.sales',
+            "Credit"    dec(15, 2)     path 'lax $.credit',
+            "Balance"   dec(15, 2)     path 'lax $.balance',
+            "Date"      varchar(10)    path 'lax $.date'
+        )
+    )
+  ) x; 
