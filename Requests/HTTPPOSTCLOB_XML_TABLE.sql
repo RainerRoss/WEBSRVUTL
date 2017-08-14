23193871146f099cb95bb5738bd796ab235e1fbf
Rainer Ross <rainer_ross@web.de>
2017-08-14 11:59:00 +0200

    Create HTTPPOSTCLOB_XML_TABLE.sql

---
+++ Requests/HTTPPOSTCLOB_XML_TABLE.sql
@@ -0,0 +1,16 @@
+SELECT x.* FROM
+	XMLTABLE('data/item' PASSING
+	xmlparse(Document SYSTOOLS.HTTPPOSTCLOB('http://172.16.0.100:8014/myapp/websrv02.pgm','','id=1'))
+	COLUMNS
+		Id         Integer      path 'id',
+		Name       varchar(40)  path 'name',
+		Country    varchar(2)   path 'country',
+		City       varchar(40)  path 'city',
+		Street     varchar(40)  path 'street', 
+		Sales      dec(15, 2)   path 'sales',
+		Credit     dec(15, 2)   path 'credit',
+		Balance    dec(15, 2)   path 'balance',
+		Date       varchar(10)  path 'date'
+
+  )
+ as x;
